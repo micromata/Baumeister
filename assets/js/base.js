7ec67e779a5e721da0701abf00378ce17025f70a
Michael Kühnel <m.kuehnel@micromata.de>
2014-06-26 14:56:58 +0200

    Update base.js - Add console methods for old IEs

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,8 @@
+/**
+ * @file  Base JavaScript needed independent of the project
+ * @author Michael Kühnel m.kuehnel@micromata.de>
+ */
+
 // Fix viewport issues with IE 10.
 // See http://getbootstrap.com/getting-started/#support-ie10-width
 if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
@@ -9,3 +14,26 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 	);
 	document.querySelector('head').appendChild(msViewportStyle);
 }
+
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+	var method;
+	var noop = function () {};
+	var methods = [
+		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		'timeStamp', 'trace', 'warn'
+	];
+	var length = methods.length;
+	var console = (window.console = window.console || {});
+
+	while (length--) {
+		method = methods[length];
+
+		// Only stub undefined methods.
+		if (!console[method]) {
+			console[method] = noop;
+		}
+	}
+}());
