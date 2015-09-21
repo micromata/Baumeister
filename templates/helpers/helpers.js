11a74e67425e37313663de856d2c6ab2ae7a2577
Michael Kühnel <m.kuehnel@micromata.de>
2015-09-21 20:16:40 +0200

    DocBlock comment for our example helper
    Related to #24

--- templates/helpers/helpers.js
+++ templates/helpers/helpers.js
@@ -3,6 +3,14 @@
  * @author Michael Kühnel <m.kuehnel@micromata.de>
  */
 
+/**
+ * Adds the current year to a string. Divides given string and year by a space.
+ * @param {String} s The string which should be suffixed with the current year.
+ * @returns {String} The given string suffixed with the current year.
+ * @example:
+ * {{addYear '©'}} --> © 2015
+ *
+ */
 var addYear = function (s) {
 	return s + ' ' + new Date().getFullYear();
 };
