e6fb69b5cd945ceb5876ee7368f2be93fcbee9c7
René Viering <r.viering@micromata.de>
2016-03-18 14:21:00 +0100

    Refactor example react code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Component = React.createClass({ // eslint-disable-line no-unused-vars
+const SimpleComponentWithoutLogic = React.createClass({ // eslint-disable-line no-unused-vars
+	render() {
+		return <p>Hello, my name is {this.props.name}</p>;
+	}
+});
+
+const getInitialData = () => Array(2000).fill(0).map((item, index) => 'Item ' + index);
+
+const SimpleFilter = React.createClass({ // eslint-disable-line no-unused-vars
 	getInitialState() {
-		return {counter: 0};
+		return {data: getInitialData(), filter: ''};
 	},
 
-	componentDidMount() {
-
+	renderListItems() {
+		return this.state.data
+			.filter(d => d !== '' && d.startsWith(this.state.filter))
+			.map(item => <li key={item}>{item}</li>);
 	},
 
-	handleOnClick() {
-		this.setState({counter: this.state.counter + 1});
+	handleOnChange() {
+		this.setState({filter: this.refs.filterText.value});
 	},
 
 	render() {
-		return <div onClick={this.handleOnClick}>
-			Hello, React YaY! {this.state.counter}
+		return <div>
+			<input type="text" onChange={this.handleOnChange} ref="filterText"/>
+			<ul>
+				{this.renderListItems()}
+			</ul>
+		</div>;
+	}
+});
+
+const Component = React.createClass({ // eslint-disable-line no-unused-vars
+	getInitialState() {
+		return {counter: 0, text: 'Data-binding is very hard to implement ^^'};
+	},
+
+	handleIncrement() {
+		this.setState({counter: this.state.counter + 1});
+	},
+
+	handleDecrement() {
+		this.setState({counter: this.state.counter - 1});
+	},
+
+	onInputChanged() {
+		this.setState({text: this.refs.textInput.value});
+	},
+
+	render() {
+		return <div className="container">
+			<h1>Hello, React!</h1>
+			{this.state.counter}
+			<br/>
+			<a className="btn btn-primary" href="#" role="button" onClick={this.handleIncrement}>Increment</a>
+			<a className="btn btn-danger" href="#" role="button" onClick={this.handleDecrement}>Decrement</a>
+			<br/>
+			<br/>
+			<h1>{this.state.text}</h1>
+			<input className="form-control" onChange={this.onInputChanged} value={this.state.text} ref="textInput"/>
+			<SimpleComponentWithoutLogic name="René Viering"/>
+			<SimpleFilter/>
 		</div>;
 	}
 });
