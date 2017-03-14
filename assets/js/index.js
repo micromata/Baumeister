76783ce40a74ae0ee24635cf3ec9cace3d13a5f0
Nicolai Süper <n.sueper@micromata.de>
2017-03-14 15:33:31 +0100

    brought base.js back (fixes #91)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import base from './base';
 // this is necessary because bootstrap itself checks the existence of jQuery with window.jQuery.
 window.jQuery = $;
 
@@ -7,6 +8,7 @@ require('bootstrap');
 require('select2');
 
 $(() => {
+	base();
 	console.log('YaY, my first ES6-Module !!!!');
 	$('select').select2();
 });
