e7a3757889ce3cf0daab2d372ad1733c4c12766a
Michael Kühnel <m.kuehnel@micromata.de>
2017-03-22 13:08:43 +0100

    Fix linting errors introcuded by dependency updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 import $ from 'jquery';
-// this is necessary because bootstrap itself checks the existence of jQuery with window.jQuery.
+// This is necessary because bootstrap itself checks the existence of jQuery with window.jQuery.
 window.jQuery = $;
 
-// Because of bootstrap is not an UMD Module, we cannot import it with es6 syntax.
+// Because of bootstrap is not an UMD Module, we can’t import it with ES6 syntax.
 require('bootstrap');
 
 $(() => {
