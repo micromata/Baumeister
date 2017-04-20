6a855d08634f69ac49f50b9bfea886a5f6c8bfa8
Nicolai Süper <nsueper@micromata.de>
2017-04-20 12:03:12 +0200

    Add less compiling, sourcemap generation, minifying and autoprefixing for dev and prod build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,59 @@
 import gulp from 'gulp';
+// Style processing
+import less from 'gulp-less';
+import cleanCss from 'gulp-clean-css';
+import sourcemaps from 'gulp-sourcemaps';
+import Autoprefix from 'less-plugin-autoprefix';
+// Util
+import rename from 'gulp-rename';
 import del from 'del';
 
-const paths = {
-	styles: {},
-	scripts: {},
-	images: {}
+const isProdBuild = () => process.argv.filter(val => val.toLowerCase().indexOf('-prod') !== -1).length > 0;
+
+const sources = {
+	styles: './src/assets/less/index.less',
+	scripts: '',
+	images: ''
 };
 
-export function cleanDev() {
-	del(['server']);
+const destinations = {
+	dev: {
+		styles: './server/assets/css/',
+		scripts: '',
+		images: ''
+	},
+	prod: {
+		styles: './dist/assets/css',
+		scripts: '',
+		images: ''
+	}
+};
+
+export function clean() {
+	if (isProdBuild()) {
+		return del(['dist']);
+	} else {
+		return del(['server']);
+	}
+}
+
+export function styles() {
+	if (isProdBuild()) {
+		return gulp.src(sources.styles)
+			.pipe(less({
+				plugins: [new Autoprefix()]
+			}))
+			.pipe(cleanCss())
+			.pipe(rename({
+				suffix: '.min'
+			}))
+			.pipe(gulp.dest(destinations.prod.styles));
+	}
+	return gulp.src(sources.styles)
+		.pipe(sourcemaps.init())
+		.pipe(less({
+			plugins: [new Autoprefix()]
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(destinations.dev.styles));
 }
