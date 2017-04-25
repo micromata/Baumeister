df80169dd34bd3ece351e7beb347c581c750399f
Nicolai Süper <nsueper@micromata.de>
2017-04-25 10:38:59 +0200

    Refactor settings to own config file

---
+++ gulp.config.js
@@ -0,0 +1,45 @@
+export const pkgJson = require('./package.json');
+
+export const mainDirectories = {
+	dev: './server/',
+	dist: './dist/'
+};
+
+export const settings = {
+	sources: {
+		markup: ['./src/*.html'],
+		styles: ['./src/assets/less/**/*.less'],
+		stylesEntryPoint: './src/assets/less/index.less',
+		scripts: ['./src/app/**/*.js'],
+		images: ['./src/assets/img/**/*.{png,jpg,gif,svg}'],
+		externalCss: pkgJson.bootstrapKickstart.bundleCSS,
+		externalJs: pkgJson.bootstrapKickstart.bundleExternalJS,
+		staticFiles: pkgJson.bootstrapKickstart.includeStaticFiles
+	},
+	destinations: {
+		dev: {
+			markup: `${mainDirectories.dev}`,
+			styles: `${mainDirectories.dev}assets/css/`,
+			scripts: `${mainDirectories.dev}app/`,
+			images: `${mainDirectories.dev}assets/img/`,
+			libs: `${mainDirectories.dev}libs/`
+		},
+		prod: {
+			markup: `${mainDirectories.dist}`,
+			styles: `${mainDirectories.dist}assets/css/`,
+			scripts: `${mainDirectories.dist}app/`,
+			images: `${mainDirectories.dist}assets/img/`,
+			libs: `${mainDirectories.dist}libs/`
+		}
+	},
+	autoPrefix: {
+		browsers: [
+			'> 1%',
+			'last 3 version',
+			'ie 8',
+			'ie 9',
+			'Firefox ESR',
+			'Opera 12.1'
+		]
+	}
+};
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,51 +23,10 @@ import notify from 'gulp-notify';
 import plumber from 'gulp-plumber';
 import processhtml from 'gulp-processhtml';
 import uncss from 'gulp-uncss';
+import {settings, mainDirectories, pkgJson} from './gulp.config';
 
 const isProdBuild = () => process.argv.filter(val => val.toLowerCase().indexOf('-prod') !== -1).length > 0;
-const pkgJson = require('./package.json');
 const server = browserSync.create();
-const mainDirectories = {
-	dev: './server/',
-	dist: './dist/'
-};
-
-const settings = {
-	sources: {
-		markup: ['./src/*.html'],
-		styles: ['./src/assets/less/**/*.less'],
-		stylesEntryPoint: './src/assets/less/index.less',
-		scripts: ['./src/app/**/*.js'],
-		images: ['./src/assets/img/**/*.{png,jpg,gif,svg}'],
-		externalCss: pkgJson.bootstrapKickstart.bundleCSS,
-		externalJs: pkgJson.bootstrapKickstart.bundleExternalJS,
-		staticFiles: pkgJson.bootstrapKickstart.includeStaticFiles
-	},
-	destinations: {
-		dev: {
-			markup: `${mainDirectories.dev}`,
-			styles: `${mainDirectories.dev}assets/css/`,
-			scripts: `${mainDirectories.dev}app/`,
-			images: `${mainDirectories.dev}assets/img/`,
-			libs: `${mainDirectories.dev}libs/`
-		},
-		prod: {
-			markup: `${mainDirectories.dist}`,
-			styles: `${mainDirectories.dist}assets/css/`,
-			scripts: `${mainDirectories.dist}app/`,
-			images: `${mainDirectories.dist}assets/img/`,
-			libs: `${mainDirectories.dist}libs/`
-		}
-	},
-	autoPrefix: [
-		'> 1%',
-		'last 3 version',
-		'ie 8',
-		'ie 9',
-		'Firefox ESR',
-		'Opera 12.1'
-	]
-};
 
 function onError(err) {
 	notify({
@@ -93,9 +52,7 @@ export function styles() {
 		return gulp.src(settings.sources.stylesEntryPoint)
 			.pipe(plumber({errorHandler: onError}))
 			.pipe(less({
-				plugins: [new Autoprefix({
-					browsers: settings.autoPrefix
-				})]
+				plugins: [new Autoprefix(settings.autoPrefix)]
 			}))
 			.pipe(cleanCss())
 			.pipe(rename({
@@ -113,9 +70,7 @@ export function styles() {
 		.pipe(plumber({errorHandler: onError}))
 		.pipe(sourcemaps.init())
 		.pipe(less({
-			plugins: [new Autoprefix({
-				browsers: settings.autoPrefix
-			})]
+			plugins: [new Autoprefix(settings.autoPrefix)]
 		}))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(settings.destinations.dev.styles));
@@ -172,7 +127,7 @@ export function vendorScripts() {
 }
 
 export function bundleExternalCSS(done) {
-	const files = require('./package.json').bootstrapKickstart.bundleCSS;
+	const files = pkgJson.bootstrapKickstart.bundleCSS;
 	if (files.length < 1) {
 		return done();
 	}
