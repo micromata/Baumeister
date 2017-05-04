7a9d3a90511f4dbf22aa05d337858e4b59a533eb
Michael Kühnel <m.kuehnel@micromata.de>
2017-05-05 00:35:20 +0200

    fix(eslint): bring back eslints fix option

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,30 +1,48 @@
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
+import gulpIf from 'gulp-if';
+import es from 'event-stream';
 
 import {settings} from '../config';
 import onError from '../onError';
 import {isProdBuild} from '../commandLineArgs';
+
+function isFixed(file) {
+	return file.eslint != null && file.eslint.fixed;
+}
+
 /**
  * ESLint task:
  * Run `gulp lint` respectively `gulp lint --production`
  */
-function lint() {
+function lint(done) {
 	if (isProdBuild()) {
-		return gulp.src([...settings.sources.scripts, './*.js'])
+		return gulp.src([...settings.sources.scripts, './*.js', './gulp/**/*.js'])
 			.pipe(eslint())
 			.pipe(eslint.format())
 			.pipe(eslint.failAfterError());
 	}
-	/**
-	 * TODO: Run ESLint with --fix for dev build
-	 * Seems that isn’t possible to fix files in place with multiple inputs. See example.
-	 * Looks like we would need to use https://github.com/robrich/gulp-exec to execute ESLint (or xo) with the fix option.
-	 */
-	return gulp.src([...settings.sources.scripts, './*.js'])
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError())
-		.on('error', onError);
+
+	return es.concat(
+		gulp.src(settings.sources.scripts)
+			.pipe(eslint({fix: true}))
+			.pipe(eslint.format())
+			.pipe(gulpIf(isFixed, gulp.dest('./src/app/')))
+			.pipe(eslint.failAfterError())
+			.on('error', onError),
+		gulp.src('./*.js')
+			.pipe(eslint({fix: true}))
+			.pipe(eslint.format())
+			.pipe(gulpIf(isFixed, gulp.dest('./')))
+			.pipe(eslint.failAfterError())
+			.on('error', onError),
+		gulp.src('./gulp/**/*.js')
+			.pipe(eslint({fix: true}))
+			.pipe(eslint.format())
+			.pipe(gulpIf(isFixed, gulp.dest('./gulp/')))
+			.pipe(eslint.failAfterError())
+			.on('error', onError)
+	).on('end', done);
 }
 lint.description = '`gulp lint` lints JavaScript via ESLint';
 
