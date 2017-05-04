6a267ee1268474a065ff511a80ec2a3f23372352
Michael Kühnel <m.kuehnel@micromata.de>
2017-05-04 22:30:56 +0200

    fix(browserify): Delete browserify caches before build
    Needed to make sure changes made in `.babelrc` will be applied.

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -1,16 +1,29 @@
 import del from 'del';
+import path from 'path';
 
 import {mainDirectories} from '../config';
 import {isProdBuild} from '../commandLineArgs';
+import {clientCacheFile} from './clientScripts';
+import {vendorCacheFile} from './vendorScripts';
 
+const browserifyCache = [
+	path.join(__dirname, `/../../${clientCacheFile}`),
+	path.join(__dirname, `/../../${vendorCacheFile}`)
+];
 /**
  * Delete output directories
  */
 function clean() {
 	if (isProdBuild()) {
-		return del(mainDirectories.dist);
+		return del([
+			...browserifyCache,
+			mainDirectories.dist
+		]);
 	}
-	return del(mainDirectories.dev);
+	return del([
+		...browserifyCache,
+		mainDirectories.dev
+	]);
 }
 
 export default clean;
