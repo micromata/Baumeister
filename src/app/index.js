9c5fad24d8329d5c910a7b10895bee226dcede13
Nicolai Süper <n.sueper@micromata.de>
2017-05-10 14:47:59 +0200

    refactor: remove bootstrap dep and use bootstrap-sass dep now

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import {consoleErrorFix, ieViewportFix} from './base';
 // This is necessary because bootstrap itself checks the existence of jQuery with window.jQuery.
 global.jQuery = $;
 // Because of bootstrap is not an UMD Module, we can’t import it with ES6 syntax.
-require('bootstrap');
+require('bootstrap-sass');
 
 $(() => {
 	consoleErrorFix();
