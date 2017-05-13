c357a49065d978ab08cb7b8052e9a58a076370bc
Michael Kühnel <m.kuehnel@micromata.de>
2017-05-13 22:16:04 +0200

    chore(ESLint): Lint files in gulp directory
    … and remove TODO comment (see #164).

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -10,17 +10,13 @@ import {isProdBuild} from '../commandLineArgs';
  */
 function lint() {
 	if (isProdBuild()) {
-		return gulp.src([...settings.sources.scripts, './*.js'])
+		return gulp.src([...settings.sources.scripts, './*.js', './gulp/**/*.js'])
 			.pipe(eslint())
 			.pipe(eslint.format())
 			.pipe(eslint.failAfterError());
 	}
-	/**
-	 * TODO: Run ESLint with --fix for dev build
-	 * Seems that isn’t possible to fix files in place with multiple inputs. See example.
-	 * Looks like we would need to use https://github.com/robrich/gulp-exec to execute ESLint (or xo) with the fix option.
-	 */
-	return gulp.src([...settings.sources.scripts, './*.js'])
+
+	return gulp.src([...settings.sources.scripts, './*.js', './gulp/**/*.js'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError())
