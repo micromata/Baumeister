fdc19d05ad0fd11f62a77f51bf2c717ec405dbb6
Nicolai Süper <n.sueper@micromata.de>
2017-07-12 10:22:58 +0200

    refactor: Use PostCSS and plugins for CSS transforms in styles task
    Closes #191

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,16 +2,15 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
-import uncss from 'gulp-uncss';
-import vendorPrefix from 'gulp-autoprefixer';
-import cleanCss from 'gulp-clean-css';
+import uncss from 'uncss';
+import cssnano from 'cssnano';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
 import rename from 'gulp-rename';
 
-import {settings, useHandlebars} from '../config';
+import {settings} from '../config';
 import {isProdBuild} from '../commandLineArgs';
 import onError from '../onError';
-
-const htmlSources = useHandlebars ? './.metalsmith-build/*.html' : settings.sources.html;
 
 const autoPrefixOptions = {
 	browsers: [
@@ -31,21 +30,24 @@ function styles() {
 	if (isProdBuild()) {
 		return gulp.src(settings.sources.stylesEntryPoint)
 			.pipe(sass())
-			.pipe(vendorPrefix(autoPrefixOptions))
-			.pipe(cleanCss({level: {1: {specialComments: 0}}}))
+			.pipe(postcss([
+				autoprefixer(autoPrefixOptions),
+				cssnano({
+					discardComments: {
+						removeAll: true
+					}
+				})]))
 			.pipe(rename('index.min.css'))
 			.pipe(gulp.dest(settings.destinations.prod.styles))
 			.pipe(rename('index.uncss.min.css'))
-			.pipe(uncss({
-				html: htmlSources
-			}))
+			.pipe(postcss([uncss.postcssPlugin({html: `${settings.destinations.prod.html}/**/*.html`})]))
 			.pipe(gulp.dest(settings.destinations.prod.styles));
 	}
 	return gulp.src(settings.sources.stylesEntryPoint)
 		.pipe(plumber({errorHandler: onError}))
 		.pipe(sourcemaps.init())
 		.pipe(sass())
-		.pipe(vendorPrefix(autoPrefixOptions))
+		.pipe(postcss([autoprefixer(autoPrefixOptions)]))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(settings.destinations.dev.styles));
 }
