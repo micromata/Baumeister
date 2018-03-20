dc76071b7222b1dd33d795485c17821373e2f7b9
Michael Kühnel <m.kuehnel@micromata.de>
2018-03-20 01:00:23 +0100

    refactor(webpack): Handle differences between dev and prod build in one config

--- build/webpack.config.babel.js
+++ build/webpack.config.babel.js
@@ -4,11 +4,15 @@ import globby from 'globby';
 import webpack from 'webpack';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
+import PurifyCSSPlugin from 'purifycss-webpack';
+import ImageminPlugin from 'imagemin-webpack-plugin';
 import WebpackAssetsManifest from 'webpack-assets-manifest';
 import minimist from 'minimist';
 import {stripIndents} from 'common-tags';
 
-import {settings, useHandlebars} from './config';
+import {generateBanners, mainDirectories, settings, useHandlebars} from './config';
+const pkg = require('../package.json');
 const configFile = require('../baumeister.json');
 
 const cliFlags = minimist(process.argv.slice(2));
@@ -40,13 +44,26 @@ const getVendorCSS = function () {
 	return {vendor: vendorCSS};
 };
 
+const purifyCSSOptions = {
+	paths: globby.sync(path.join(settings.destinations.handlebars, '**/*.html')),
+	purifyOptions: {
+		minify: true,
+		cleanCssOptions: {level: {1: {specialComments: 0}}},
+		whitelist: configFile.purifyCSS.whitelist
+	}
+};
+
 if (!cliFlags.json) {
 	console.log(chalk.yellow(stripIndents`Build target: ${chalk.bold.inverse(buildTarget)}
 	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`));
 }
 
-module.exports = (options) => ({
-	devServer: options.devServer,
+module.exports = {
+	devServer: {
+		contentBase: isDevMode ? path.join(__dirname, mainDirectories.dev) : path.join(__dirname, mainDirectories.prod),
+		port: isDevMode ? 3000 : 3001,
+		overlay: true
+	},
 	entry: {
 		app: `${settings.sources.app}index.js`,
 		...getVendorCSS()
@@ -102,7 +119,11 @@ module.exports = (options) => ({
 			}
 		]
 	},
-	output: options.output,
+	output: {
+		path: isDevMode ? path.join(__dirname, mainDirectories.dev) : path.join(__dirname, mainDirectories.prod),
+		filename: configFile.cacheBusting ? 'app/[name].[chunkhash].bundle.js' : 'app/[name].bundle.js',
+		chunkFilename: configFile.cacheBusting ? 'app/[name].[chunkhash].bundle.js' : 'app/[name].bundle.js'
+	},
 	plugins: [
 		manifest,
 		generateCssFile,
@@ -132,11 +153,28 @@ module.exports = (options) => ({
 			},
 			...copyVendorFiles
 		]),
-		...options.plugins
+		new webpack.DefinePlugin(isDevMode ? {...configFile.webpack.DefinePlugin.development} : {...configFile.webpack.DefinePlugin.production}),
+		isDevMode ? new webpack.SourceMapDevToolPlugin({columns: false}) : function () {},
+		isDevMode ? function () {} : new ImageminPlugin({test: /\.(jpe?g|png|gif|svg)$/i}),
+		isDevMode ? function () {} : new UglifyJSPlugin({
+			uglifyOptions: {
+				compress: {
+					drop_console: true, // eslint-disable-line camelcase
+					drop_debugger: true // eslint-disable-line camelcase
+				}
+			}
+		}),
+		configFile.purifyCSS.usePurifyCSS && !isDevMode ? new PurifyCSSPlugin(purifyCSSOptions) : function () {},
+		generateBanners && !isDevMode ? new webpack.BannerPlugin({
+			banner: stripIndents`${pkg.title} - v${pkg.version}
+			${pkg.author.email}
+			Copyright ©${new Date().getFullYear()} ${pkg.author.name}
+			${new Date().toLocaleDateString('en-US', {day: '2-digit', month: 'long', year: 'numeric'})}`
+		}) : function () {}
 	],
 	stats: {
 		timings: true,
 		version: false,
 		hash: false
 	}
-});
+};
